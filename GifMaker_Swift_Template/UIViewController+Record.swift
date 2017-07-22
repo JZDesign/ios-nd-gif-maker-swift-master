9faaa1191cb3d188d3eb42b4c39ea3a512a8d48d
JZDesign <jacobzivandesign@gmail.com>
2017-07-22 13:45:52 -0500

    Convert to Swift from GifMaker_Obj

--- GifMaker_Swift_Template/UIViewController+Record.swift
+++ GifMaker_Swift_Template/UIViewController+Record.swift
@@ -17,7 +17,7 @@ let loopCount = 0 // 0 = loop forever
 
 extension UIViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    @IBAction func launchVideCamer(sender: AnyObject) {
+    @IBAction func launchVideCamer() {
         // create image picker 
         let recordVideoController = UIImagePickerController()
         recordVideoController.sourceType = UIImagePickerControllerSourceType.camera
@@ -28,13 +28,53 @@ extension UIViewController : UIImagePickerControllerDelegate, UINavigationContro
         present(recordVideoController, animated: true, completion: nil)
     }
     
+    @IBAction func presentVideoOptions() {
+        if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+            doPhotoLibrary()
+
+        } else {
+            let gifActionSheet = UIAlertController(title: "Create new Gif", message: nil, preferredStyle: .actionSheet)
+            let recordVideo = UIAlertAction(title: "Record a Video", style: .default, handler: { (UIAlertAction) in
+                self.launchVideCamer()
+            })
+            
+            let chooseFromExisting = UIAlertAction(title: "Choose from Existing", style: .default, handler: { (UIAlertAction) in
+                self.doPhotoLibrary()
+            })
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            
+            gifActionSheet.addAction(recordVideo)
+            gifActionSheet.addAction(chooseFromExisting)
+            gifActionSheet.addAction(cancel)
+            present(gifActionSheet, animated: true, completion: nil)
+            //adjust button colors in action sheet
+            let pink = UIColor(red: 255.0/255.0, green: 65.0/255.0, blue: 112.0/255.0, alpha: 1)
+            gifActionSheet.view.tintColor = pink
+            
+        }
+        
+    }
+    
+    
+    // MARK: Launch Photo Library
+    
+    func doPhotoLibrary() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.mediaTypes = [kUTTypeMovie as String]
+        picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        present(picker, animated: true, completion: nil)
+    }
+    
     
     // MARK: ImagePickerDelegate
     
     public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
-        let mediaType = info[UIImagePickerControllerMediaURL] as! String
         
+        //let mediaType = info[UIImagePickerControllerMediaURL] as! String
+        let mediaType = info[UIImagePickerControllerMediaType] as! String
         if mediaType == kUTTypeMovie as String {
             let videoURL = info[UIImagePickerControllerMediaURL] as! URL
             convertVideoToGif(videoURL: videoURL)
